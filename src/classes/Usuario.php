58ffc6de567f3f126ccc95d1a0733b259a478453
Manuel Alarcon <alarmanuel16@gmail.com>
2024-11-28 14:02:55 +0100

    Profile Portfolio Shown

--- src/classes/Usuario.php
+++ src/classes/Usuario.php
@@ -2,15 +2,27 @@
 
     class Usuario{
 
+        public int $id; 
         public string $nombre ;
         public string $apellido ;
         public string $correo ;
         public string | null $perfil ;
         public string | null $descripcion ;
-        public int      $id; 
-        public int      $edad ; 
+        public int $edad ; 
         public array | null $funcion ; 
-
+        
+        /**
+         * __construct
+         *
+         * @param  mixed $id
+         * @param  mixed $nombre
+         * @param  mixed $apellido
+         * @param  mixed $correo
+         * @param  mixed $perfil
+         * @param  mixed $edad
+         * @param  mixed $descripcion
+         * @return void
+         */
         public function __construct($id, $nombre, $apellido, $correo, $perfil, $edad, $descripcion){
 
             $this->id = $id ;
--- src/subpages/profile.php
+++ src/subpages/profile.php
@@ -1,6 +1,39 @@
 <?php 
     session_start(); // Inicializa la sesión
     require_once "../classes/Usuario.php" ;
+
+    $usuario = unserialize($_SESSION["_usuario"]) ;
+
+    $publicaciones = [] ;
+
+    try {
+
+        $sqli = new mysqli("chorus_db", "root", "", "chorus_db") ;            
+
+    } catch(mysqli_sql_exception $excepcion) {
+
+        die() ;
+
+    }
+
+    $sql = "SELECT IdPublic, TituloPublic, TextoPublic, ImagenPublic FROM portfolio
+            WHERE IdUsu = '{$usuario->id}'" ;
+
+        $datos = $sqli->query($sql) ;
+
+        if ($datos->num_rows > 0):
+
+            for($i = 0 ; $i < $datos->num_rows ; $i++):
+
+                $publ = $datos->fetch_assoc();
+
+                array_push($publicaciones, $publ) ;
+
+            endfor ;
+
+        endif
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,9 +61,9 @@
 
     <div class="container-fluid text-center">
         <div class="row">
-            <div class="col-3 bg-purple border-purple position-sticky pt-5 pb-5"> 
+            <div class="col-3 bg-purple border-purple altura sticky pt-5 pb-5"> 
 
-                <div class="row mt-4 mb-3">
+                <div class="row mt-2 mb-3">
 
                     <div class="col"></div>
                     <div class="col">
@@ -84,21 +117,79 @@
 
                 ?>
 
-                <div class="row mt-3 mb-3">
+                    <div class="row mt-4">
+                        <h1>Descripción</h1>
 
-                    <div class="col-2"></div>
-                    <div class="col-8">
-                        <!-- main -->
-                        <div class="card">
-                                <h3 class="p-4">Descripción</h3>
-                                <p><?= $usuario->descripcion ?></p>
+                        <div class="col-2"></div>
+                        <div class="col-8">
+                            <!-- main -->
+                            <div class="card bg-purple">
+                                    <p class="m-4"><?= $usuario->descripcion ?></p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-2"></div>
+                        <div class="col-2"></div>
 
                 </div>
 
                 <?php 
+
+                    endif;
+
+                ?>
+
+                <?php 
+
+                    if(!empty($publicaciones)):
+
+                ?>
+
+                    <h1 class="mt-5">Publicaciones</h1>
+
+
+                <?php
+
+                        for($i = 0 ; $i < count($publicaciones) ; $i++):
+
+                ?>
+
+                    <div class="row mt-4">
+                        
+                        <div class="col-2"></div>
+                        <div class="col-8">
+                            <!-- main -->
+                            <div class="card bg-purple">
+                                <div class="row">
+                                    <h4 class="pt-4"><?= $publicaciones[$i]["TituloPublic"] ?></h4>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-1"></div>
+                                    <div class="col-10">
+                                    <p class="m-4"><?= $publicaciones[$i]["TextoPublic"] ?></p>
+                                    </div>
+                                    <div class="col-1"></div>
+                                </div>
+
+                                <?php if($publicaciones[$i]["ImagenPublic"]): ?>
+
+                                    <div class="row pb-3">
+                                        <div class="col"></div>
+                                        <div class="col">
+                                            <img src="<?= $publicaciones[$i]["ImagenPublic"] ?>" alt="">
+                                        </div>
+                                        <div class="col"></div>
+                                    </div>
+
+                                <?php endif ; ?>
+                            </div>
+                        </div>
+                        <div class="col-2"></div>
+
+                </div>
+
+                <?php 
+
+                        endfor ;
 
                     endif;
 
